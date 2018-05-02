845d38fd0e8e4014ceb300b87e6a59eb566428b2
Anders Darander <anders@chargestorm.se>
2018-05-02 15:28:10 +0200

    blowrak: change LED behaviour
    Signed-off-by: Anders Darander <anders@chargestorm.se>

--- keyboards/ergodox_ez/keymaps/blowrak_programming/keymap.c
+++ keyboards/ergodox_ez/keymaps/blowrak_programming/keymap.c
@@ -281,12 +281,25 @@ void matrix_scan_user(void)
 
   uint8_t layer = biton32(layer_state);
 
-  if (layer == GAME)
-  {
-    ergodox_right_led_1_on();
-  }
-  else
-  {
-    ergodox_right_led_1_off();
+  ergodox_board_led_off();
+  ergodox_right_led_1_off();
+  ergodox_right_led_2_off();
+  ergodox_right_led_3_off();
+
+  switch (layer) {
+  case BASE:
+    break;
+  case SHFT:
+        ergodox_right_led_1_on();
+    break;
+  case NAVI:
+    ergodox_right_led_2_on();
+    break;
+  case NAVI_TWO:
+    ergodox_right_led_2_on();
+    break;
+  case GAME:
+    ergodox_right_led_3_on();
+    break;
   }
 };
