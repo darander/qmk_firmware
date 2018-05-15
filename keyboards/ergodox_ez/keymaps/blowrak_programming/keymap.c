d4c9f122077c076404c85ffc56743f8e5e206aca
Anders Darander <anders@chargestorm.se>
2018-05-15 08:23:47 +0200

    Add mouse wheel up and down to NAVI
    for my Ergodox EZ blowrak layout.
    
    Signed-off-by: Anders Darander <anders@chargestorm.se>

--- keyboards/ergodox_ez/keymaps/blowrak_programming/keymap.c
+++ keyboards/ergodox_ez/keymaps/blowrak_programming/keymap.c
@@ -172,7 +172,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [NAVI] = LAYOUT_ergodox(
        // left hand
        VRSN, KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,
-       EPRM, KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_TRNS,
+       EPRM, KC_TRNS, KC_WH_U, KC_MS_U, KC_WH_D, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS,
        KC_LSFT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN3, KC_BTN2,
